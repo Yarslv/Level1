2f75c6bbdd169b1831708c7bd0e6fadc1fa42794
y <yarik.prodan@gmail.com>
2021-09-06 23:58:33 +0300

    Add some functional

--- app/src/main/java/com/yprodan/level1/AuthActivity.kt
+++ app/src/main/java/com/yprodan/level1/AuthActivity.kt
@@ -1,11 +1,40 @@
 package com.yprodan.level1
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import com.yprodan.level1.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener {
+            if ("""(\w)+(.)(\w)+(@)(\w)+[.][a-z]{2,3}""".toRegex()
+                    .containsMatchIn(binding.emailInputEditText.text.toString())
+                && !binding.passwordEditText.text.isNullOrEmpty()
+            ) {
+                Log.d("auth", "ok")
+                startActivity(Intent(this, MainActivity::class.java).apply {
+                    this.putExtra(Constants.EMAIL_TAG, binding.emailInputEditText.text.toString())
+                })
+                finish()
+            }
+        }
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+
+
+        return super.onCreateView(name, context, attrs)
     }
 }
--- app/src/main/java/com/yprodan/level1/MainActivity.kt
+++ app/src/main/java/com/yprodan/level1/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.yprodan.level1
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import com.yprodan.level1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        intent.apply {
+            with(extras?.get(Constants.EMAIL_TAG).toString()){
+                var firstName = this.subSequence(0, this.indexOf('.')).toString()
+                firstName = firstName[0].uppercase().plus(if(firstName.length > 1) firstName.subSequence(1, firstName.length) else "")
+                var lastName = this.subSequence(this.indexOf('.')+1, this.indexOf('@')).toString()
+                lastName = lastName[0].uppercase().plus(if(lastName.length > 1) lastName.subSequence(1, lastName.length) else "")
+                binding.nameTextView.text =  firstName.plus(" ").plus(lastName)//todo in strings
+            }
+        }
     }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+               return super.onCreateView(name, context, attrs)
+    }
+
 }
